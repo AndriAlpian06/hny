c3cb33f0fc49f3520ee10b8d2fba4b9063fa2861
AndriAlpian06 <61491000+AndriAlpian06@users.noreply.github.com>
2023-12-31 20:45:17 +0700

    first update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Particles from "react-particles";
+import { loadFireworksPreset } from "tsparticles-preset-fireworks"
+import { Typewriter } from "react-simple-typewriter";
+import Countdown from "react-countdown";
 
 function App() {
+  const [newYearMessage, setNewYearMessage] = useState(["Bye 2023"])
+
+  const particlesInitialization = async(engine) => {
+    await loadFireworksPreset(engine)
+  }
+
+  function toTimeNewYear(){
+    const newYearDate = new Date("January 1, 2024 00:00:00").getTime()
+    const nowDate = new Date().getTime()
+    const remainingTime = newYearDate - nowDate
+    return remainingTime
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Particles 
+        init={particlesInitialization}
+        options={{preset: "fireworks"}}
+      />
+      <div className="flex flex-col justify-center items-center min-h-screen gap-4">
+        <span className="text-white text-4xl font-bold px-4 z-50">
+          <Typewriter words={newYearMessage} loop={false} cursorStyle={"_"} cursor />
+        </span>
+        <div className="z-50 text-white font-bold text-2xl">
+          <Countdown date={Date.now() + toTimeNewYear()} onComplete={() => setNewYearMessage(["SELAMAT","TAHUN", "BARU","2024"])} />
+        </div>
+      </div>
+    </>
   );
 }
 
